04819b5432b86c20ad500bdfb03f8623c000021c
Gustavo Leonildo de Souza <gustavoleonildodesouza@MacBook-Air-de-Gustavo.local>
2025-09-17 22:05:04 -0300

    feat:create get file in supabase storage

--- src/modules/files/files.controller.ts
+++ src/modules/files/files.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Res } from '@nestjs/common';
+import type { Response } from 'express';
+import { join } from 'path';
 
+import { FilesService } from './files.service';
 @Controller('files')
-export class FilesController {}
+export class FilesController {
+  constructor(private readonly filesService: FilesService) {}
+
+  @Get('/get-file/:id')
+  async downloadFile(@Param('id') id: string, @Res() res: Response) {
+    const file = await this.filesService.findById(id);
+    if (!file) {
+      return res.status(404).json({ message: 'File not found' });
+    }
+    return res.download(join(process.cwd(), file.url));
+  }
+}
--- src/modules/files/files.service.ts
+++ src/modules/files/files.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from 'generated/prisma';
+import { createClient } from '@supabase/supabase-js';
 
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+
+if (!supabaseUrl || !supabaseServiceRoleKey) {
+  throw new Error('Supabase environment variables are not set.');
+}
+
+const supabase = createClient(supabaseUrl, supabaseServiceRoleKey);
 @Injectable()
-export class FilesService {}
+export class FilesService {
+  private prisma = new PrismaClient();
+
+  async findById(id: string) {
+    return this.prisma.file.findUnique({ where: { id } });
+  }
+
+  async uploadFileToSupabase(file: Express.Multer.File) {
+    const filePath = `invoices/${Date.now()}_${file.originalname}`;
+    const { error } = await supabase.storage
+      .from('invoices')
+      .upload(filePath, file.buffer, {
+        contentType: file.mimetype,
+        upsert: true,
+      });
+
+    if (error) {
+      throw new Error('Error uploading file to Supabase:');
+    }
+
+    const { data: publicUrlData } = supabase.storage
+      .from('invoices')
+      .getPublicUrl(filePath);
+
+    return publicUrlData?.publicUrl;
+  }
+}
